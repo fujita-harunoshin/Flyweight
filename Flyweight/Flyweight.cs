d1f5b2e16aa305cde8112ef52a08f9e6035b36b9
Harunoshin Fujita <fujita.harunoshin@gmail.com>
2024-06-07 04:13:12 +0900

    プロジェクト ファイルを追加します。

---
+++ Flyweight/Flyweight.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Flyweight;
+
+internal class Flyweight(Car car)
+{
+    private Car _sharedState = car;
+
+    public void Operation(Car uniqueState)
+    {
+        var s = JsonConvert.SerializeObject(_sharedState);
+        var u = JsonConvert.SerializeObject(uniqueState);
+        Console.WriteLine($"Flyweight: Displaying shared {s} and unique {u} state.");
+    }
+}
---
+++ Flyweight/FlyweightFactory.cs
@@ -0,0 +1,59 @@
+﻿namespace Flyweight;
+
+internal class FlyweightFactory
+{
+    private List<Tuple<Flyweight, string>> flyweights = [];
+
+    public FlyweightFactory(params Car[] args)
+    {
+        foreach (var arg in args)
+        {
+            flyweights.Add(new Tuple<Flyweight, string>(new Flyweight(arg), GetKey(arg)));
+        }
+    }
+
+    public string GetKey(Car key)
+    {
+        List<string> elements = [];
+
+        elements.Add(key.Model);
+        elements.Add(key.Color);
+        elements.Add(key.Company);
+
+        if (key.Owner != null && key.Number != null)
+        {
+            elements.Add(key.Number);
+            elements.Add(key.Owner);
+        }
+
+        elements.Sort();
+
+        return string.Join("_", elements);
+    }
+
+    public Flyweight GetFlyweight(Car sharedState)
+    {
+        var key = GetKey(sharedState);
+
+        if (!flyweights.Where(t => t.Item2 == key).Any())
+        {
+            Console.WriteLine("FlyweightFactory: Can't find a flyweight, creating new one.");
+            flyweights.Add(new Tuple<Flyweight, string>(new Flyweight(sharedState), key));
+        }
+        else
+        {
+            Console.WriteLine("FlyweightFactory: Reusing existing flyweight.");
+        }
+        return flyweights.Where(t => t.Item2 == key).FirstOrDefault().Item1;
+    }
+
+    public void ListFlyweights()
+    {
+        var count = flyweights.Count;
+        Console.WriteLine($"\nFlyweightFactory: I have {count} flyweights:");
+        foreach (var flyweight in flyweights)
+        {
+            Console.WriteLine(flyweight.Item2);
+        }
+    }
+}
